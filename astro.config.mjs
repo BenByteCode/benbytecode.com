cd46b0b2d29b7267d1df76e41ece96858aeadfe9
Okjoon Kim <okjoon.kim@gmail.com>
2025-11-03 07:10:53 -0800

    add amplify.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    site: 'https://benbytecode.com',
+    base: '/',
+    output: 'dist',
     plugins: [tailwindcss()]
   }
 });
