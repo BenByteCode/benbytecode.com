535706ac2796e34fe71b15e6635da4d36fc16a96
Okjoon Kim <okjoon.kim@gmail.com>
2025-11-02 17:22:46 -0800

    update the layout

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        benpurple: "#6A5ACD",
+      },
+    },
+  },
+};
