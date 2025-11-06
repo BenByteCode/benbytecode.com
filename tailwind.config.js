57d2f30a2a898697a937fbc0b5ee5d1281eb17ff
Okjoon Kim <okjoon.kim@gmail.com>
2025-11-06 10:40:28 -0800

    update the project settings and structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        benpurple: "#6A5ACD",
+      fontFamily: {
+        'mono': ['JetBrains Mono', 'Monaco', 'Consolas', 'monospace'],
       },
+      colors: {
+        'code-blue': '#61dafb',
+        'code-green': '#98d982',
+        'code-purple': '#c792ea',
+        'ben-purple': "#6A5ACD",
+
+      }
     },
   },
-};
+  plugins: [],
+}
