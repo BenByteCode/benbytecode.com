5a7de095f875eda40eb86cf4f53cfbb9e779e150
Okjoon Kim <okjoon.kim@gmail.com>
2025-11-06 09:42:31 -0800

    update the configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://benbytecode.com',
+  base: '/',
+  outDir: './dist',
+
+  // Vite specific configuration remains here
   vite: {
-    site: 'https://benbytecode.com',
-    base: '/',
-    outDir: 'dist',
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss()
+    ]
   }
 });
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  base: './',
-  plugins: [tailwindcss()],
-});
